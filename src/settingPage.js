e780e4de5f9aa2c5ae8d33707b96d17c0b0dea21
Maksim Ashmiana <max.basic.94@gmai.com>
2021-11-19 17:01:55 +0300

    added location div and some styles for it

--- src/settingPage.js
+++ src/settingPage.js
@@ -1,7 +1,33 @@
+function createLocationDescriptionDiv(location) {
+    const locationDescriptionDiv = document.createElement('div');
+    locationDescriptionDiv.className = 'locationDescriptionDiv';
+    locationDescriptionDiv.textContent = `${location.name}, ${location.country}`;
+    return locationDescriptionDiv;
+}
+
+function createLocationCaptionDiv() {
+    const locationCaptionDiv = document.createElement('div');
+    locationCaptionDiv.textContent = 'Your Location Now'
+    locationCaptionDiv.className = 'locationCaptionDiv';
+    return locationCaptionDiv;
+}
+
+function createLocationDiv(location) {
+    const locationDiv = document.createElement('div');
+    const locationCaptionDiv = createLocationCaptionDiv();
+    const locationDescriptionDiv = createLocationDescriptionDiv(location);
+    locationDiv.className = 'locationDiv';
+    locationDiv.append(locationCaptionDiv);
+    locationDiv.append(locationDescriptionDiv);
+    return locationDiv
+}
+
 function createPage(data) {
     const {current, location} = data;
+    const locationDiv = createLocationDiv(location);
     console.log(current);
     console.log(location);
+    document.querySelector('.settingPage').append(locationDiv);
 }
 
 async function getData() {
