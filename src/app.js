60658d509cd47f35bf7231e633c6d0c3af1daa38
Maksim Ashmiana <max.basic.94@gmai.com>
2021-11-25 10:10:55 +0300

    added page not found

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ import createHomePage from "./pages/homePage";
 import createfavouritePage from "./pages/favouritePage";
 import createSettingPage from "./pages/settingPage";
 import setColorControlButtons from "./colorControlButtons"
-// import createNotFaundPageDiv from "./notFaundPage";
+import createNotFaundPageDiv from "./pages/notFaundPage";
 
 const url = `http://api.weatherapi.com/v1/forecast.json?key=0ca217e793694cf3b27105654211511&q=auto:ip&days=4&aqi=no&alerts=no`;
 const app = document.querySelector(".app");
 const homeDiv = createHomePage(url);
 const favouriteDiv = createfavouritePage();
 const settingDiv = createSettingPage();
-// const notFaundPage = createNotFaundPageDiv();
+const notFaundPage = createNotFaundPageDiv();
 
 const routes = {
   '#home': homeDiv,
   '#favourite': favouriteDiv,
-  '#setting': settingDiv
+  '#setting': settingDiv,
+  '#notFaund': notFaundPage
 }
 
 function onRouteChanged() {
