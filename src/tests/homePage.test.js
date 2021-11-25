be6fc62e8fc8f64448afa2c98699c6a31c53491f
Maksim Ashmiana <max.basic.94@gmai.com>
2021-11-25 14:52:04 +0300

    run npm install --save-dev jest-transform-stub

--- src/tests/homePage.test.js
+++ src/tests/homePage.test.js
@@ -1,6 +1,13 @@
-import createHomePage from '../pages/homePage'
+/**
+ * @jest-environment jsdom
+ */
 
-test('test', () => {
-    expect(createHomePage(`http://api.weatherapi.com/v1/forecast.json?key=0ca217e793694cf3b27105654211511&q=auto:ip&days=4&aqi=no&alerts=no`))
-    .toBe(3);
+import createHomePage from "../pages/homePage";
+
+test("test", () => {
+  expect(
+    createHomePage(
+      `http://api.weatherapi.com/v1/forecast.json?key=0ca217e793694cf3b27105654211511&q=auto:ip&days=4&aqi=no&alerts=no`
+    )
+  ).toBe(3);
 });
