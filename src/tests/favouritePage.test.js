eef06a6ef1ec5415f00f67091f3d3a83365f9a3e
Maksim Ashmiana <max.basic.94@gmai.com>
2021-11-26 16:14:13 +0300

    added unit test for favouritePage

---
+++ src/tests/favouritePage.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import "regenerator-runtime/runtime";
+import createfavouritePage from '../pages/favouritePage'
+window.alert = jest.fn();
+
+test("homePage has className = `informer`", () => {
+  window.alert.mockClear();
+  const testPage = createfavouritePage().getAttribute("class");
+  expect(testPage).toBe("favouritePage");
+});
